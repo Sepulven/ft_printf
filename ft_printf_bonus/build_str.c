b51fdca0b5d506356411f3106792dbcb66394c8f
Arthur Aguiar <sepulven40028922@gmail.com>
2023-12-02 18:49:08 +0000

    Add -, 0 and width specifiers to the project. Need to fix some KO in the tester for the respectivy specifiers. Must fix the error with the %c converter characters.

--- ft_printf_bonus/build_str.c
+++ ft_printf_bonus/build_str.c
@@ -6,7 +6,7 @@
 /*   By: asepulve <asepulve@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 17:50:13 by asepulve          #+#    #+#             */
-/*   Updated: 2023/12/01 16:43:33 by asepulve         ###   ########.fr       */
+/*   Updated: 2023/12/02 18:40:22 by asepulve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,7 +76,7 @@ int		build_str(t_mod *mod, va_list arg)
 	len = 0;
     str = conversion(mod->conversion, arg);
 	str = mod_flags(str, mod);
-	// mod_width(str, mod);
+	str = mod_width(str, mod);
 	// mod_precision(str, mod);
     len = ft_strlen(str);
 	if (mod->conversion == 'c' && len == 0)
--- ft_printf_bonus/mod_width.c
+++ ft_printf_bonus/mod_width.c
@@ -6,14 +6,85 @@
 /*   By: asepulve <asepulve@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 13:16:24 by asepulve          #+#    #+#             */
-/*   Updated: 2023/12/01 13:22:18 by asepulve         ###   ########.fr       */
+/*   Updated: 2023/12/02 18:43:20 by asepulve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./ft_printf.h"
 
+char    *mod_minus(char *str, t_mod *mod)
+{
+    char *new;
+    int str_len;
+    int j;
+
+    str_len = ft_strlen(str);
+    j = 0;
+    new = ft_calloc(mod->min_width + 1, 1);
+    if (!new)
+        return (NULL);
+    while (j < str_len)
+    {
+        new[j] = str[j];
+        j++;
+    }
+    while (j < mod->min_width)
+        new[j++] = ' ';
+    if (str)
+        free(str);
+    return (new);
+}
+
+char    *mod_zero(char *str, t_mod *mod)
+{
+    char *new;
+    int i;
+    int j;
+
+    i = ft_strlen(str) - 1;
+    j = mod->min_width - 1;
+    new = ft_calloc(mod->min_width + 1, 1);
+    if (!new)
+        return (NULL);
+    while (i >= 0)
+        new[j--] = str[i--];
+    while (j >= 0)
+        new[j--] = '0';
+    if (str)
+        free(str);
+    return (new);
+}
+
+char    *mod_pad(char *str, t_mod *mod)
+{
+    char *new;
+    int i;
+    int j;
+
+    i = ft_strlen(str) - 1;
+    j = mod->min_width - 1;
+    new = ft_calloc(mod->min_width + 1, 1);
+    if (!new)
+        return (NULL);
+    while (i >= 0)
+        new[j--] = str[i--];
+    while (j >= 0)
+        new[j--] = ' ';
+    if (str)
+        free(str);
+    return (new);
+}
+
+
 char    *mod_width(char *str, t_mod *mod)
 {
-    (void)mod;
+    if (ft_strlen(str) >= (size_t)mod->min_width)
+        return (str);
+    if (ft_strchr(mod->flags, '-'))
+        str = mod_minus(str, mod);
+    else if (mod->flags[0] == '0')
+        str = mod_zero(str, mod);
+    else
+        str = mod_pad(str, mod);
     return (str);
 }
